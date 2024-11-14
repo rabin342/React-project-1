57536bf3c300ebbe978831de391b0d1682edb53c
Rabin Regmi <rabinregmi121@gmail.com>
2024-11-14 15:12:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import HeroSection from "./Components/Hero";
+import Navigation from "./Components/Navigation";
+
+const App = () => {
+
+
+return (
+<div>
+    <Navigation />
+    <HeroSection />
+    
+</div>
+ );
+};
+export default App;
